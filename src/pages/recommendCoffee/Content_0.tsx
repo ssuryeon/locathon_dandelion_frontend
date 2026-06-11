e90d738968742c1819c693d021c5bd65d3ed414f
Leesuryeon <lees8351@naver.com>
2026-06-12 01:30:00 +0900

    fix: 빌드 오류 수정

--- src/pages/recommendCoffee/Content_0.tsx
+++ src/pages/recommendCoffee/Content_0.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import { ContentContainer } from '../../components/ContentContainer';
 import { useNavigate } from 'react-router';
 import { SelectBtn } from '../../components/SelectBtn';
